6cda8c4e5d90805150f78fb3d7dc73dcaf0edf02
uzn1023 <e5305@gm.kochi-ct.jp>
2024-02-18 17:45:18 +0900

    version

--- paperbot.py
+++ paperbot.py
@@ -214,7 +214,7 @@ with tab_search:
     )
     search_message = st.chat_input("PDFに関する質問を入力してください", key='unique_key_2')
     if search_message:
-        retriever = database.as_retriever(search_type="mmr", search_kwargs={'fetch_k': 50, "k": 10, , 'lambda_mult': 0.25})
+        retriever = database.as_retriever(search_type="mmr", search_kwargs={'fetch_k': 50, "k": 10, 'lambda_mult': 0.25})
         docs = retriever.get_relevant_documents(search_message, k=10)
         st.markdown(f"##  \"{search_message}\" の検索結果")
         for num, source_document in enumerate(docs):
